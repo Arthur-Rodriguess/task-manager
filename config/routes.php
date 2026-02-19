ad89fe0623fdfbdaac033f249e592009daccd9f4
Arthur-Rodriguess <artrod150309@gmail.com>
2026-02-19 19:13:35 -0300

    feat(routes): adiciona rotas de autenticação e refatora as rotas

--- config/routes.php
+++ config/routes.php
@@ -1,20 +1,63 @@
 <?php
 
-use TaskManager\Controllers\{
+use TaskManager\Controllers\{LoginController,
+	LoginFormController,
+	LogoutController,
+	RegisterController,
+	RegisterFormController,
 	TaskAddController,
 	TaskConcludeController,
 	TaskEditController,
 	TaskFormController,
 	TaskListController,
-	TaskRemoveController
-};
+	TaskRemoveController};
 
 return [
-	"GET|/" => TaskListController::class,
-	"GET|/new-task" => TaskFormController::class,
-	"POST|/new-task" => TaskAddController::class,
-	"GET|/edit-task" => TaskFormController::class,
-	"POST|/edit-task" => TaskEditController::class,
-	"GET|/remove-task" => TaskRemoveController::class,
-	"GET|/conclude-task" => TaskConcludeController::class
+	"GET|/" => [
+		'controller' => TaskListController::class,
+		'auth' => true
+	],
+	"GET|/new-task" => [
+		'controller' => TaskFormController::class,
+		'auth' => true
+	],
+	"POST|/new-task" => [
+		'controller' => TaskAddController::class,
+		'auth' => true
+	],
+	"GET|/edit-task" => [
+		'controller' => TaskFormController::class,
+		'auth' => true
+	],
+	"POST|/edit-task" => [
+		'controller' => TaskEditController::class,
+		'auth' => true
+	],
+	"GET|/remove-task" => [
+		'controller' => TaskRemoveController::class,
+		'auth' => true
+	],
+	"GET|/conclude-task" => [
+		'controller' => TaskConcludeController::class,
+		'auth' => true
+	],
+	"GET|/login" => [
+		'controller' => LoginFormController::class,
+		'auth' => false
+	],
+	"POST|/login" => [
+		'controller' => LoginController::class,
+		'auth' => false
+	],
+	"GET|/register" => [
+		'controller' => RegisterFormController::class,
+		'auth' => false
+	],
+	"POST|/register" => [
+		'controller' => RegisterController::class,
+	],
+	"GET|/logout" => [
+		'controller' => LogoutController::class,
+		'auth' => true
+	]
 ];
