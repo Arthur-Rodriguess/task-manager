6ff3279f5efcbb4c22f12d314ce3e89f93d90e9e
Arthur-Rodriguess <artrod150309@gmail.com>
2026-02-21 12:39:01 -0300

    feat(routes): adiciona sessão e muda a forma de definir o controller necessário

--- public/index.php
+++ public/index.php
@@ -8,11 +8,18 @@ use TaskManager\Controllers\TaskEditController;
 use TaskManager\Controllers\TaskFormController;
 use TaskManager\Controllers\TaskListController;
 use TaskManager\Controllers\TaskRemoveController;
+use TaskManager\Middleware\AuthMiddleware;
 use TaskManager\Model\Repository\TaskRepository;
+use TaskManager\Model\Repository\UserRepository;
+
 date_default_timezone_set('America/Fortaleza');
 $dbPath = __DIR__ . "/../database.sqlite";
 $pdo = new PDO("sqlite:$dbPath");
 $taskRepository = new TaskRepository($pdo);
+$userRepository = new UserRepository($pdo);
+
+session_start();
+session_regenerate_id();
 
 $routes = require_once __DIR__ . "/../config/routes.php";
 
@@ -21,8 +28,30 @@ $httpMethod = $_SERVER['REQUEST_METHOD'];
 $key = "$httpMethod|$pathInfo";
 
 if(array_key_exists($key, $routes)) {
-	$controllerClass = $routes[$key];
-	$controller = new $controllerClass($taskRepository);
+	$route = $routes[$key];
+
+	if($route['auth'] === true) {
+		AuthMiddleware::handle();
+	}
+
+	$controllerClass = $route['controller'];
+	$reflection = new ReflectionClass($controllerClass);
+	$constructor = $reflection->getConstructor();
+
+	if($constructor) {
+		$params = $constructor->getParameters();
+		if(count($params) > 0) {
+			$dependency = $params[0];
+			$dependencyType = $dependency->getType()->getName();
+
+			$controller = match ($dependencyType) {
+				TaskRepository::class => new $controllerClass($taskRepository),
+				UserRepository::class => new $controllerClass($userRepository)
+			};
+		} else {
+			$controller = new $controllerClass();
+		}
+	}
 } else {
 	$controller = new Error404Controller();
 }
