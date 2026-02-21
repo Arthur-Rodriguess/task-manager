46f38b141645c0f93fcaa08701226ce99c869958
Arthur-Rodriguess <artrod150309@gmail.com>
2026-02-21 12:32:12 -0300

    fix(view): altera texto incorreto do formulário

--- views/register-form.php
+++ views/register-form.php
@@ -1,11 +1,12 @@
 <?php require_once __DIR__ . "/header-html.php"; ?>
+<?php require_once __DIR__ . "/aside-html.php"; ?>
 	<main>
-		<h1 class="form-title">Realize o seu Login</h1>
+		<h1 class="form-title">Realize o seu cadastro</h1>
 		<form method="post" class="form">
 			<input type="text" name="username" placeholder="Nome do usuário">
 			<input type="email" name="email" placeholder="E-mail do usuário">
 			<input type="password" name="password" placeholder="Senha do usuário">
-			<input type="submit" id="submit-button" value="Entrar">
+			<input type="submit" id="submit-button" value="Criar">
 			<p class="form-text">Já criou uma conta? <a href="/login">Faça seu login</a></p>
 		</form>
 	</main>
