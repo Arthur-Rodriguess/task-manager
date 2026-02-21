89479ae2fe3f6fb83a1c738145dc2301c6a57f59
Arthur-Rodriguess <artrod150309@gmail.com>
2026-02-21 12:36:24 -0300

    feat(view): adiciona funcionalidade de nome de usuário dinâmico e atualiza a vizualização

--- views/header-html.php
+++ views/header-html.php
@@ -14,7 +14,17 @@
             <img src="img/task-manager-logo.png" alt="header logo" width="50" class="header-logo">
             <h1 class="header-title">TASK<br>MANAGER</h1>
         </a>
-        <a href="/new-task" class="new-task-link">
-            <img src="img/new-task.png" alt="new task icon" width="40">
-        </a>
+        <?php if(!empty($_SESSION['user'])): ?>
+            <div class="header-actions">
+                <a href="/new-task" class="new-task-link">
+                    <img src="img/new-task.png" alt="new task icon" width="35">
+                </a>
+                <div class="user-menu">
+                    <button id="userToggle" class="user-toggle">
+                        <img src="img/avatar.png" alt="avatar" width="42">
+                        <span class="user-name"><?= $_SESSION['user']->name?></span>
+                    </button>
+                </div>
+            </div>
+        <?php endif; ?>
     </header>
