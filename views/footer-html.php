c0154071ee205ca74557c28356f6ac8258f34a34
Arthur-Rodriguess <artrod150309@gmail.com>
2026-02-21 12:37:26 -0300

    feat(view): adiciona animação para sidebar

--- views/footer-html.php
+++ views/footer-html.php
@@ -1,2 +1,17 @@
+<script>
+    const toggle = document.getElementById('userToggle');
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+
+    toggle.addEventListener('click', () => {
+        sidebar.classList.add('open');
+        overlay.classList.add('show');
+    });
+
+    overlay.addEventListener('click', () => {
+        sidebar.classList.remove('open');
+        overlay.classList.remove('show');
+    });
+</script>
 </body>
 </html>
