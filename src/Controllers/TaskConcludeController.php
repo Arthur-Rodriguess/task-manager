35a3c83cdc0ce795434d03bb0b86989327407001
Arthur-Rodriguess <artrod150309@gmail.com>
2026-02-21 12:51:18 -0300

    refactor(controller): muda a forma de tratar tarefas já concluídas

--- src/Controllers/TaskConcludeController.php
+++ src/Controllers/TaskConcludeController.php
@@ -6,7 +6,7 @@ use TaskManager\Controllers\Controller;
 use TaskManager\Helpers\Status;
 use TaskManager\Model\Repository\TaskRepository;
 
-class TaskConcludeController implements Controller
+class TaskConcludeController extends AuthenticatedController
 {
 	public function __construct(private TaskRepository $taskRepository)
 	{
@@ -14,20 +14,32 @@ class TaskConcludeController implements Controller
 
 	public function processaRequisicao(): void
 	{
+		$userId = $this->requireLogin();
+		$this->taskRepository->expireTasks();
+
 		$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
-		$task = $this->taskRepository->find($id);
+		$task = $this->taskRepository->find($id, $userId);
 		if ($task === null) {
 			header("Location: /?success=0");
 			exit();
 		}
 
 		$taskStatus = $task->getStatus();
-		if ($taskStatus === Status::Concluded || $taskStatus === Status::Expired) {
+		if ($taskStatus === Status::Expired) {
 			header("Location: /?success=0");
 			exit();
 		}
 
-		if($this->taskRepository->conclude($task->id)) {
+		if ($taskStatus === Status::Concluded) {
+			if($this->taskRepository->resetTask($id, $userId)) {
+				header("Location: /?success=1");
+				exit();
+			}
+			header("Location: /?success=0");
+			exit();
+		}
+
+		if($this->taskRepository->conclude($task->id, $task->userId)) {
 			header("Location: /?success=1");
 		} else {
 			header("Location: /?success=0");
